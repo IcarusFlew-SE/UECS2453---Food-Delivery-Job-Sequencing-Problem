5f14db0899fd53a455584fa9fe412e4b625833f4
bryanmartyn-software-engineer <bryanmartyn@1utar.my>
2026-04-08 21:25:03 +0800

    abstract need to change to handle int for deadline

--- Food_Delivery/src/service/AbstractScheduler.java
+++ Food_Delivery/src/service/AbstractScheduler.java
@@ -1,16 +1,38 @@
 package service;
+
 import jobModel.Jobs;
 import java.util.*;
 import algoStrategies.JobSeqStrategy;
 
-public abstract class AbstractScheduler<T extends Jobs> implements JobSeqStrategy<T>{
-	//Sort by profit
-	protected void sortByProfit(List<T> jobs) {
-		jobs.sort((a, b) -> Double.compare(b.getProfit(), a.getProfit()));
-	}
-	// By Deadline
-	protected int getMaxDeadline(List<T> jobs) {
-		return 0;
-	}
-	
+public abstract class AbstractScheduler<T extends Jobs> implements JobSeqStrategy<T> {
+    
+    // Sort by profit (descending)
+    protected void sortByProfit(List<T> jobs) {
+        jobs.sort((a, b) -> Double.compare(b.getProfit(), a.getProfit()));
+    }
+    
+    // Get maximum deadline - FIX THIS METHOD
+    protected int getMaxDeadline(List<T> jobs) {
+        return jobs.stream()
+                   .mapToInt(Jobs::getDeadline)  // Now works if deadline is int
+                   .max()
+                   .orElse(0);
+    }
 }
+
+// package service;
+// import jobModel.Jobs;
+// import java.util.*;
+// import algoStrategies.JobSeqStrategy;
+
+// public abstract class AbstractScheduler<T extends Jobs> implements JobSeqStrategy<T>{
+	//Sort by profit
+	// protected void sortByProfit(List<T> jobs) {
+		// jobs.sort((a, b) -> Double.compare(b.getProfit(), a.getProfit()));
+	// }
+	// By Deadline
+	// protected int getMaxDeadline(List<T> jobs) {
+		// return 0;
+	// }
+	
+// }
