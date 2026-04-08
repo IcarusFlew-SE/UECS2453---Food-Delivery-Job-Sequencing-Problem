c2c9abe02caa2bb4935e6917f831bf9f3024c7ae
IcarusFlew-SE <samalexson1214@gmail.com>
2026-04-09 00:26:36 +0800

    GreedyAlgo check

--- Food_Delivery/src/service/AbstractScheduler.java
+++ Food_Delivery/src/service/AbstractScheduler.java
@@ -19,20 +19,3 @@ public abstract class AbstractScheduler<T extends Jobs> implements JobSeqStrateg
                    .orElse(0);
     }
 }
-
-// package service;
-// import jobModel.Jobs;
-// import java.util.*;
-// import algoStrategies.JobSeqStrategy;
-
-// public abstract class AbstractScheduler<T extends Jobs> implements JobSeqStrategy<T>{
-	//Sort by profit
-	// protected void sortByProfit(List<T> jobs) {
-		// jobs.sort((a, b) -> Double.compare(b.getProfit(), a.getProfit()));
-	// }
-	// By Deadline
-	// protected int getMaxDeadline(List<T> jobs) {
-		// return 0;
-	// }
-	
-// }
