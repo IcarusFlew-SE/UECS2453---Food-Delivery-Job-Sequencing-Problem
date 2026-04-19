79c0793db7182e1f65ec77ca974e75dbef3b4d5a
kzz300 <khzhengyan@gmail.com>
2026-04-19 22:30:00 +0800

    update

--- Food_Delivery/src/algoStrategies/DynamicProgramming.java
+++ Food_Delivery/src/algoStrategies/DynamicProgramming.java
@@ -1,66 +1,64 @@
 package algoStrategies;
 import java.util.*;
-import jobModel.jobs;
+import jobModel.Jobs;
 import service.AbstractScheduler;
 
 
 public class DynamicProgramming extends AbstractScheduler {
 	
-	public DynamicProgramming(List<List> jobs) {
+	public DynamicProgramming(List<Jobs> jobs) {
 		super(jobs);
 	}
 	
-	@overide 
+	@Override 
 	public String getAlgorithm() {
-		return "DynamicProgramming";
+		return "Dynamic Programming";
 	}
 	
-	@overide
+	@Override
 	public Result<Jobs> schedule(List<Jobs> jobs){
-		int n = job.size();
+		int n = jobs.size();
 		int maxDeadline = getMaxDeadline(jobs);
 		
+		List<Jobs> sortedJobs = new ArrayList<>(jobs);
+		sortedJobs.sort(Comparator.comparingInt(Jobs::getDeadline));
+		
 		//Create dynamic programming  table
-		double [][] dp = new double [n + 1]{[maxDeadline + 1];
+		double[][] dp = new double[n + 1][maxDeadline + 1];
 		
 		//Build table
-		for(int i +1; i<= n; i++) {
-			Jobs job = jobs.get(i-1);
-			for(int t =1; i <= maxDeadline; t++) {
-				if(t >= 1) 
-				{
-				dp[i][t] = Math.max(
-						dp[i - 1][t],
-						dp[i -1 ][t - 1] + job.getProfit()
-						);					
-				} else {
-					dp[i][t] = dp[i + 1][t];
+		for(int i = 1; i <= n; i++) {
+			Jobs job = sortedJobs.get(i - 1);
+			for(int t = 1; t <= maxDeadline; t++) { 
+				
+				if(t <= job.getDeadline()) {
+					dp[i][t] = Math.max(dp[i - 1][t], dp[i - 1][t - 1] + job.getProfit());
+				}else {
+					dp[i][t] = Math.max(dp[i - 1][t], dp[i][t - 1]);
 				}
-			}
-		}
+			} 
+		}	
 		
 		//Find selected jobs
 		List<Jobs> selected = new ArrayList<>();
-		int = t maxDeadline;
+		int t = maxDeadline;
 		
-		for(int = 1; i > 0; i--) {
+		for(int i = n; i > 0; i--) {
 			if(dp[i][t] != dp[i - 1][t]) {
-				Jobs job = jobs.get(i - 1);
+				Jobs job = sortedJobs.get(i - 1);
 				selected.add(job);
-				t -= 1;
+				t = Math.min(t, job.getDeadline()) - 1;
 			}
 		}
 		
-		Collection.reverse(selected);
+		Collections.reverse(selected);
 		
 		//Get rejected jobs
 		List<Jobs> rejected = getUnselectedJobs(jobs, selected);
-		double totalPorfit = totalProfit(selected);
+		double totalProfit = totalProfit(selected);
 		
 		return new Result<>(selected, rejected, totalProfit);
 		
 		}
  		
 	}
-
-}
